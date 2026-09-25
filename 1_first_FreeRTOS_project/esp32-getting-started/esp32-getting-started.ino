7cfeafa0b829c7e75dbc56b094703e8b44546e9d
nik-etl <niklas.etling@gmail.com>
2026-09-25 14:43:10 +0300

    Added assignment one

--- 1_first_FreeRTOS_project/esp32-getting-started/esp32-getting-started.ino
+++ 1_first_FreeRTOS_project/esp32-getting-started/esp32-getting-started.ino
@@ -2,27 +2,30 @@
 #define RGB_BRIGHTNESS 50 
  
 void setup() { 
-  // No separate initialisation is required for this example. 
+  Serial.begin(115200);
 } 
  
 void loop() { 
   // Red ON 
-  neopixelWrite(RGB_BUILTIN, 0, 50, 0); 
-  delay(1000); 
+  neopixelWrite(RGB_BUILTIN, 59, 50, 0); 
+  Serial.println("Blue");
+  delay(200); 
  
   // LED OFF 
   neopixelWrite(RGB_BUILTIN, 0, 0, 0); 
-  delay(1000); 
+  delay(200); 
 
     // blue ON 
   neopixelWrite(RGB_BUILTIN, 0, 0, 50); 
-  delay(1000); 
+  Serial.println("Green");
+  delay(100); 
  
   // LED OFF 
   neopixelWrite(RGB_BUILTIN, 0, 0, 0); 
-  delay(1000);
+  delay(100);
     // White ON 
   neopixelWrite(RGB_BUILTIN, 50, 50, 50); 
+  Serial.println("White");
   delay(1000); 
  
   // LED OFF 
